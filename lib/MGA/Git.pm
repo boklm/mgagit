boklm/mgagit

commit 17ad237cd599702f805122508a0dc3bafdc020d0

Add the repo_conf and origin_conf functions in templates
And remove the duplication of options from main repo definition to each
repo definition. Now that we can use the repo_conf function this is no
longer useful.

lib/MGA/Git.pm
@@ -43,7 +43,7 @@ sub load_gitrepos_dir {
             my $name = "$infos->{prefix}/$bname";
             $r->{repos}{$name} = LoadFile("$infos->{include_dir}/$file");
             $r->{repos}{$name}{name} = $bname;
-            @{$r->{repos}{$name}}{keys %$infos} = values %$infos;
+            $r->{repos}{$name}{origin} = $infos->{origin};
         } elsif ($file =~ m/(.+)\.group$/ && exists $infos->{group_prefix}) {
             my $bname = $1;
             my $name = $infos->{group_prefix} . $bname;
@@ -62,17 +62,33 @@ sub load_gitrepos {
     my ($r) = @_;
     $r->{repos} = {};
     foreach my $repodef (@{$config->{repos_config}}) {
+        $repodef->{origin} = $repodef->{prefix};
         if ($repodef->{include_dir} || $repodef->{git_url}) {
             load_gitrepos_dir($r, $repodef);
         }
         foreach my $repo ($repodef->{repos} ? @{$repodef->{repos}} : ()) {
             my $name = "$repodef->{prefix}/$repo->{name}";
+            $repo->{origin} = $repodef->{origin};
             $r->{repos}{$name} = $repo;
-            my %infos = %$repodef;
-            delete $infos{repos};
-            @{$r->{repos}{$name}}{keys %infos} = values %infos;
         }
     }
+}
+
+sub origin_config {
+    my ($r, $reponame, $confname) = @_;
+    my $origin = $r->{repos}{$reponame}{origin};
+    foreach my $repodef (@{$config->{repos_config}}) {
+        next unless ($repodef->{prefix} eq $origin);
+        return $repodef->{$confname} if defined $repodef->{$confname};
+        last;
+    }
+    return $config->{$confname};
+}
+
+sub repo_config {
+    my ($r, $reponame, $confname) = @_;
+    my $res = $r->{repos}{$reponame}{$confname};
+    return defined $res ? $res : origin_config($r, $reponame, $confname);
 }
 
 sub get_ldap {
@@ -151,6 +167,8 @@ sub gitolite_repo_config {
         r      => $r,
         repo   => $repo,
         config => $config,
+        repo_conf   => sub { repo_config($r, $repo, @_) },
+        origin_conf => sub { origin_config($r, $repo, @_) },
     };
     return process_tmpl($r->{repos}{$repo}{gl_template}, 'gl', $vars);
 }

commit 2eabb7b3e7e3b452ebf0a227eea9956dd136fff9

Fix bug introduced by commit 17ad237cd599702f
The gl_template option is now in the origin_config instead of
repo_config before, so we need to use the origin_config function to
access it.

lib/MGA/Git.pm
@@ -170,7 +170,7 @@ sub gitolite_repo_config {
         repo_conf   => sub { repo_config($r, $repo, @_) },
         origin_conf => sub { origin_config($r, $repo, @_) },
     };
-    return process_tmpl($r->{repos}{$repo}{gl_template}, 'gl', $vars);
+    return process_tmpl(origin_config($r, $repo, 'gl_template'), 'gl', $vars);
 }
 
 sub gitolite_group_config {